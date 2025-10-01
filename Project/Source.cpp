e30e0e0910fc376add13f7185174550913485036
Natalya Babenko <ifelseiforelif@gmail.com>
2025-10-01 20:07:47 +0300

    added header files

--- Project/Source.cpp
+++ Project/Source.cpp
@@ -2,7 +2,7 @@
 using namespace std;	
 
 int main() {
-	cout << "Hello, World!" << endl;
+	cout << "Hello,World!" << endl;
 	int m = 10;
 	cout << "Hello, World!"<<"Hello there" << endl;
 	return 0;
